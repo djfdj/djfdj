cac3723633180c55816c27343a6791dc1d504775
djfdj <yoosh1996@daum.net>
2019-09-19 21:44:51 +0900

    third

--- pa0.c
+++ pa0.c
@@ -61,29 +61,36 @@ static int parse_command(char *command, int *nr_tokens, char *tokens[]){
 	int  k = 0;
 	while(1){
 		if((command[n] != ' ') && (command[n] != '\t')){
-			while((command[scantoken] != ' ') * (command[scantoken] != '\t')){
-				if(command[scantoken] == '\0') break;
+			while((command[scantoken] != ' ') * (command[scantoken] != '\t')*(command[scantoken] != '\n')){
 				tokensize++;
 				scantoken++;
 				}
-		char* token = (char*)malloc (sizeof(char)*(tokensize+1));
-		while((command[n] != ' ') * (command[n] != '\t')){
-			if(command[n] == '\0') break;
-			token[k] = command[n];
-			k++;
-			n++; }
-		token[k] = '\0';
-		tokens[tokenscount] = token;
-		(*nr_tokens)++;
-		if(command[n] == '\0') break;
-		tokenscount++;
-		tokensize = 0;
-		k = 0;
-		}
-		if((command[n] == ' ') || (command[n]  == '\t') ){
+			char* token = (char*)malloc (sizeof(char)*(tokensize+1));
+			while((command[n] != ' ') * (command[n] != '\t')*(command[n] != '\n')){
+				token[k] = command[n];
+				k++;
+				n++;
+				}
+			token[k] ='\0';
+			tokens[tokenscount] = token;
+			(*nr_tokens)++;
+
+			tokenscount++;
+			tokensize = 0;
+			k = 0;
+			}
+		if((command[n] == ' ') || (command[n]  == '\t')  ){
 			n++;
 			scantoken++;
 		}
+		if(command[n] == '\n'){
+			tokens[tokenscount] = '\0';
+			(*nr_tokens)++;
+			break;
+
+		}
+		
+
 	}
 	return 0;
 }
